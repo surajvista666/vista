a9e2132c85716005049a825f3479a92207b60edc
Suraj Kumar Patel <surajpatel@paynet.pro>
2023-03-20 16:12:22 +0530

    third comming to find sum

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String []args)
+    {
+        int x=12;
+        int y=45;
+        int Sum;
+        Sum = x+y;
+        System.out.println(sum);
+    }
+}
